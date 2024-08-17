51e765e7db217da71d331ea41f30cc948c2a6c5b
skycader <skycader@yandex.ru>
2024-08-18 00:43:47 +0300

    main: init

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,28 @@
+import { Component, computed, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss',
+})
+export class CounterComponent {
+  public counter = signal(0);
+  public changes = signal(-1);
+  public doubled = computed(() => this.counter() * this.counter());
+  public effect = effect(
+    () => {
+      this.counter();
+      this.changes.update((changes) => changes + 1);
+    },
+    { allowSignalWrites: true },
+  );
+  public increment() {
+    this.counter.update((value) => value + 1);
+  }
+
+  public decrement() {
+    this.counter.update((value) => value - 1);
+  }
+}
